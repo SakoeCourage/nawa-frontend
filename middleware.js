d3e4498198293746111722947a8f3767aefb0187
SakoeCourage <35003252+SakoeCourage@users.noreply.github.com>
2025-06-09 13:50:13 +0000

    Update middleware.js
    Updated MiddleWare

--- middleware.js
+++ middleware.js
@@ -1,3 +1,14 @@
-export {default} from 'next-auth/middleware'
+// middleware.ts
+import { withAuth } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
-export const config = {matcher: ["/profile/:path*"]}
+export default function middleware(req) {
+  const url = req.nextUrl.pathname;
+
+  // Apply NextAuth middleware only on `/profile` routes
+  //if (url.startsWith("/profile")) {
+    //return withAuth()(req);
+  //}
+
+  return NextResponse.next();
+}
